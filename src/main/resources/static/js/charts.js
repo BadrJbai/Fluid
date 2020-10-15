b602e251954c11ae4b509b7b1a633d1993da438e
Badrjbai <badrjbai@gmail.com>
2020-10-15 17:17:42 +0100

    Added js library for my pie chart tweaked my home landing page

--- src/main/resources/static/js/charts.js
+++ src/main/resources/static/js/charts.js
@@ -1,8 +1,16 @@
-/**
- * My chart will be referenced in the landing page
- */
-var myDoughnutChart = new Chart(ctx, {
+new Chart(document.getElementById('myPiechart'), {
     type: 'doughnut',
-    data: data,
-    options: options
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {},
 });
